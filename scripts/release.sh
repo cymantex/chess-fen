ee53fd4c2c18140e5848a42f7d27c9e357b7f83f
dv16sen <dv16sen@cs.umu.se>
2019-07-26 21:12:33 +0200

    Fixed publish script.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+npm run build
 cd dist
 npm publish
---
+++ src/module/main.js
@@ -0,0 +1,23 @@
+import {Position as Position0} from "./Position.js";
+import {InvalidFenError as InvalidFenError1} from "./InvalidFenError.js";
+import {pieces as pieces2} from "./FenConstants.js";
+import {colors as colors2} from "./FenConstants.js";
+import {emptySquare as emptySquare2} from "./FenConstants.js";
+import {outsideBoard as outsideBoard2} from "./FenConstants.js";
+import {blackPieces as blackPieces2} from "./FenConstants.js";
+import {whitePieces as whitePieces2} from "./FenConstants.js";
+import {fenToPiece as fenToPiece2} from "./FenConstants.js";
+import {pieceToFen as pieceToFen2} from "./FenConstants.js";
+import {Fen as Fen3} from "./Fen.js";
+
+export const Position = Position0;
+export const InvalidFenError = InvalidFenError1;
+export const pieces = pieces2;
+export const colors = colors2;
+export const emptySquare = emptySquare2;
+export const outsideBoard = outsideBoard2;
+export const blackPieces = blackPieces2;
+export const whitePieces = whitePieces2;
+export const fenToPiece = fenToPiece2;
+export const pieceToFen = pieceToFen2;
+export const Fen = Fen3;
